694db17321076becb3c20070d4a55b5a0cf8f63b
LynchzDEV <opmok48@gmail.com>
2024-08-24 15:57:04 +0700

    refactor: Remove unused code and imports from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import BackgroundBoxesDemo from './pages/Boxes';
 import BackgroundBeamsWithCollisionDemo from './pages/Beam';
-// import BackgroundBeamsDemo from './pages/MoreBeam';
 import AuroraBackgroundDemo from './pages/Aurora';
-// import Hero from './pages/Hero';
-// import { HeroParallaxDemo } from './pages/HeroPallalax';
 import { AppleCardsCarouselDemo } from './pages/Carousel';
 import { TimelineDemo } from './pages/Timeline';
 import './index.css';
@@ -13,9 +9,6 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuroraBackgroundDemo />
-    {/* <Hero /> */}
-    {/* <BackgroundBeamsDemo /> */}
-    {/* <HeroParallaxDemo /> */}
     <AppleCardsCarouselDemo />
     <TimelineDemo />
     <BackgroundBeamsWithCollisionDemo />
